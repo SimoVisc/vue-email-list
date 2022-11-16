6ea29006803ac0717a06cef324194e06de689a09
SimoVisc <simonaviscardivs@gmail.com>
2022-11-16 17:36:35 +0100

    stampa dieci email

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emails: []
       }
+    },
+    created(){
+        for(let i =1 ; i<= 10; i ++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=>{
+            this.emails.push(response.data.response)
+        })
     }
+   }
   }).mount('#app')
